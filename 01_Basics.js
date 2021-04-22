2ab5c440548aee56c76ae0322f01446da9ecd819
StallonesHyper <stallonefernandess@gmail.com>
2021-04-22 21:21:37 +0530

    Added A Comment

--- 01_Basics.js
+++ 01_Basics.js
@@ -1,42 +1,42 @@
 // Data Types:
 
-// 1. Declaration Of Variables 
+// 1. Declaration Of Variables
 
 var myNAme = "Stallone";
 myName = "Stallone";
 
-let ourName ="Stallone Fernandes";
+let ourName = "Stallone Fernandes";
 
 const pi = 3.14;
 
-// 2. Storing Variables With The Assignment Operator 
+// 2. Storing Variables With The Assignment Operator
 
 var a = 10;
 var b = 20;
 
-b=a;
+b = a;
 console.log(a);
-  
+
 // 3. Console.Log Will Print On Console
 console.log("Stallone Fernandes");
 var myName = "Fernandes";
-console.log(myName);//Here Name Is Updated To Fernandes And Not Stallone Anymore.
+console.log(myName); //Here Name Is Updated To Fernandes And Not Stallone Anymore.
 // NOTE: JavaScript Is A Dynamically Typed Language Therefore We Can Update Or Reuse The Variable Name With Different msContentScript.
 
-
 //This Is ALso A Variable But This Is Generally Known As An Unchangeable Variable I.E Also Known As A Constant.
-const naming="STLLNE";
+const naming = "STLLNE";
 console.log(naming);
 //This Is A CONST Variable.
 
 // Data Types :
 
-let a="Stallone";
-//This Is A String 
+let a = "Stallone";
+//This Is A String
 
 let b = 20;
 //This Is A Number OF Type Integer
 
-let c= 20.5;
+let c = 20.5;
 //This Is A Float Number
 
+// Hello I Am Stallone
