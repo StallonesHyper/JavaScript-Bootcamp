3bc3132ab919d72f02af3406fe205c8c66d77914
StallonesHyper <stallonefernandess@gmail.com>
2021-06-09 23:50:07 +0530

    Asynchronous JavaScript Tutorial

---
+++ Async JS/Sync-VS-Async.js
@@ -0,0 +1,19 @@
+//NOTE: Sync JavaScript Example
+function doSomething() {
+  console.log("Do Something");
+}
+
+console.log("Do Something Function");
+doSomething();
+console.log("Done");
+
+//NOTE: Async JavaScript Code
+
+console.log("Asynchronous Code");
+
+//TIMEOUT (setTimeout)
+setTimeout(() => {
+  console.log("Time-Out Delayed ");
+}, 5000);
+
+console.log("Timed Out");
